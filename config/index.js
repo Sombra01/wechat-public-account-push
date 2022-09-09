df98d7c83ce7a7c1eca97cd1fc07ce7db759f0b1
Sombra01 <113169767+Sombra01@users.noreply.github.com>
2022-09-09 17:39:52 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,17 +31,17 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "宝贝",
+      name: "老婆0",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oAVT86gEleLjSONUknY6-vKWHrEg",
+      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "gAJ2X3xkYHekMV4iD0-lN_XFjJj-DoVkj-MP1_7kJvs",
+      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
       // 所在省份
-      province: "山东",
+      province: "广东",
       // 所在城市
-      city: "潍坊",
+      city: "肇庆",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://s.weibo.com/top/summary"
+      openUrl: "https://wangxinleo.cn"
     }, 
     {
       name: "老婆1", 
@@ -77,12 +77,12 @@ export const config = {
   CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-  CALLBACK_USERS: [Sombra0
+  CALLBACK_USERS: [
     {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "oAVT86gEleLjSONUknY6-vKWHrEg",
+      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
     }, 
   ],
     
@@ -93,9 +93,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "山东",
+  PROVINCE: "广东",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "潍坊",
+  CITY: "惠州",
 
   /** 重要节日相关 */
 
@@ -106,8 +106,10 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name":  "李小梦", "year": "1996", "date": "12-29"},
-    {"type": "生日", "name":  "高丰竣", "year": "1994", "date": "11-06"},
+    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
+    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
   ],
 
   /**
@@ -125,7 +127,13 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2022-05-20"},
+    {"keyword": "love_day", date: "2015-05-01"},
+    // 结婚纪念日
+    {"keyword": "marry_day", date: "2020-01-04"},
+    // 退伍日, 不用可以删掉
+    {"keyword": "ex_day", date: "2022-08-31"},
+    // sakana日
+    // {"keyword": "sakana_day", date: "2022-01-06"},
     // ... 
   ],
 
